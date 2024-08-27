4bddcda7362b249d2fcd4208f9c865392c341f6b
tunenj <yakubyusuf6@gmail.com>
2024-08-27 05:50:38 +0100

    modified

--- react-project/src/ToDoList.jsx
+++ react-project/src/ToDoList.jsx
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 function ToDoList(){
     const [tasks, setTasks] = useState(["Go to gym", "Take bath", "Eat breakfast"]);
     const [newTask, setNewTask] = useState("");
+    const [editIndex, setEditIndex] = useState(null);
+    const [editTask, setEditTask] = useState("");
 
     function handleInputChange(event){
         setNewTask(event.target.value);
-
     }
-    function addTask(){
 
+    function addTask(){
        if(newTask.trim() !==""){
         setTasks(t => [...t, newTask]);
         setNewTask("");
@@ -27,9 +28,35 @@ function ToDoList(){
             [updatedTasks[index], updatedTasks[index-1]] = 
             [updatedTasks[index-1],updatedTasks[index]];
             setTasks(updatedTasks);
-        }
-
+        }    
     }
+
+    function startEditing(index) {
+        setEditIndex(index);
+        setEditTask(tasks[index]);
+    }
+
+    function handleEditInputChange(event) {
+        setEditTask(event.target.value);
+    }
+
+    function saveEditTask() {
+        if (editTask.trim() !== "") {
+            const updatedTasks = tasks.map((task, index) =>
+                index === editIndex ? editTask : task
+            );
+            setTasks(updatedTasks);
+            setEditIndex(null);
+            setEditTask("");
+        }
+    }
+
+    function cancelEdit() {
+        setEditIndex(null);
+        setEditTask("");
+    }
+
+
     function moveTaskDown(index){
         if(index < tasks.length-1){
             const updatedTasks = [...tasks];
@@ -37,7 +64,6 @@ function ToDoList(){
             [updatedTasks[index+1],updatedTasks[index]];
             setTasks(updatedTasks);
         }
-
     }
 
     return(<div className="to-do-list">
@@ -49,24 +75,31 @@ function ToDoList(){
         />
         <button className="add-button" onClick={addTask}>Add</button> 
     <ol>
-        {tasks.map((task, index) =>
+        {tasks.map((task, index) => (
             <li key={index}> 
-                 <span className="text">{task}</span>
-            <button className="delete-button"
-                onClick={() => deleteTask(index)}
-                >Delete
-            </button>
-            <button className="move-button"
-                onClick={() => moveTaskUp(index)}
-                >up
-            </button>
-            <button className="move-button"
-                onClick={() => moveTaskDown(index)}
-                >Down
-            </button>
-            </li>
-    )}
-    </ol>
+               {editIndex === index ? (
+                     <div>
+                        <input 
+                            type="text" 
+                            value={editTask} 
+                            onChange={handleEditInputChange}/>
+
+                        <button className="save-button" onClick={saveEditTask}>Save</button>
+                        <button className="cancel-button" onClick={cancelEdit}>Cancel</button>
+                        </div>
+
+                         ) : (
+                        <>
+                            <span className="text">{task}</span>
+                            <button className="edit-button" onClick={() => startEditing(index)}>Edit</button>
+                            <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+                            <button className="move-button" onClick={() => moveTaskUp(index)}>Up</button>
+                            <button className="move-button" onClick={() => moveTaskDown(index)}>Down</button>
+                        </>
+                        )}
+                    </li>
+                ))}
+            </ol>
     </div>);
 
 }
